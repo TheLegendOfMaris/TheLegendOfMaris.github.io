7a47b49f6f28da5edfa78c7976bacb822f959390
marie.schwabe <marie.schwabe@stud.htwk-leipzig.de>
2025-05-17 20:20:50 +0200

    Navbar rdy

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,10 @@
-
+import Mond from '../assets/Mond.png';
 
 const Navbar = () => {
   return (
-    <nav className="sticky top-0 z-50 bg-[#1a1a24] px-6 py-4 flex justify-between items-center">
+    <nav className="fixed top-0 left-0 right-0 z-50 bg-[#1a1a24]/80 backdrop-blur-sm px-6 py-2 flex justify-between items-center">
       <div className="flex items-center gap-2">
+        <img src={Mond} alt="Mond-Logo" className="w-8 h-8" />
         <span className="font-medium">Marie Schwabe</span>
       </div>
       
@@ -17,16 +18,10 @@ const Navbar = () => {
         <a href="#projects" className="hover:text-gray-300 transition-colors">
           Projekte
         </a>
-        <button className="bg-[#13131f] hover:bg-[#232336] transition-colors px-4 py-2 rounded-md">
+        <a href="#contact" className="bg-[#000000] hover:bg-[#08071B] transition-colors px-4 py-2 rounded-md">
           Kontakt aufnehmen
-        </button>
+        </a>
       </div>
-      
-      <button className="md:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-        </svg>
-      </button>
     </nav>
   );
 };
